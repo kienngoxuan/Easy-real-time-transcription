1fa64ebfab192f8d4bd83af125bc1ae6cd15c854
Kien Ngo Xuan <ngoxuankien2005@gmail.com>
2025-10-22 15:27:35 +0700

    use faster-whisper on cpu and relevant compute type

--- Code/backend/models/load_whisper.py
+++ Code/backend/models/load_whisper.py
@@ -1,1 +1,13 @@
 #Loading and cache Whisper functions
+# backend/models/load_whisper.py
+from faster_whisper import WhisperModel
+import os
+
+_model = None
+
+def get_model(model_size: str = "tiny.en", device: str = "cpu", compute_type: str = "int8"):
+    global _model
+    if _model is None:
+        # path or model size
+        _model = WhisperModel(model_size, device=device, compute_type=compute_type)
+    return _model
